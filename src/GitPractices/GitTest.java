376552bbdf117895f481562892a74b6e968d9238
Maierdan <daniyamaridan@gmail.com>
2022-12-29 18:27:51 -0600

    I created GitTest Class

---
+++ src/GitPractices/GitTest.java
@@ -0,0 +1,13 @@
+package GitPractices;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        int num1 = 1000;
+        int num2 = 9000;
+        int num3 = 9999;
+
+        System.out.println(num1 + num2 + num3);
+    }
+}
